3c5d14376386d6e4494e63d1d4c3eff6e7233114
nolyk <nolykofficial@gmail.com>
2025-09-14 11:28:00 +0500

    Первый коммит React проекта

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import { useState } from 'react'
+import Main from './components/Main'
+import Application from './components/Application'
+import About from './components/About'
+
+import './App.css'
+import About from './components/About'
+
+function App() {
+
+  return (
+    <>
+    <Main/>
+    <About/>
+    <Application/>
+    </>
+  )
+}
+
+export default App
